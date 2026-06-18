20bef29f5620df5ab6e36ce8ec2a5417d5ce2342
Pawanraje Kadam <kep717680@gmail.com>
2026-06-18 13:23:08 +0530

    Enhance next.config.js for shader files and alias
    Added Webpack configuration for .glsl files and updated alias for Netlify.

--- next.config.js
+++ next.config.js
@@ -1,11 +1,17 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   webpack: (config) => {
+    // 1. Tells Next.js how to read .glsl shader files
     config.module.rules.push({
       test: /\.(glsl|vs|fs|vert|frag)$/,
-      type: 'asset/source', // This tells Next.js to read shader files as text
+      type: 'asset/source',
     });
+
+    // 2. Helps Netlify find the files when using the '@/' shortcut
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
 
     return config;
   },
